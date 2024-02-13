8f2591c86f85fd80b4c3d3f76f3babcc74cecd1a
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-02-14 00:27:21 +0800

    set aria-expanded attribute accordingly

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const navToggle = document.querySelector('#navToggle');
 const navList = document.querySelector('#navList');
 const toggleIcon = document.querySelector('.toggle-icon');
 
-const iconMenuClose = '/assets/images/icon-menu-close.svg';
-const iconMenuOpen = '/assets/images/icon-menu.svg';
+const iconMenuClose = './assets/images/icon-menu-close.svg';
+const iconMenuOpen = './assets/images/icon-menu.svg';
 
 navToggle.addEventListener('click', () => {
   navList.classList.toggle('opened');
 
-  navList.classList.contains('opened')
-    ? (toggleIcon.src = iconMenuClose)
-    : (toggleIcon.src = iconMenuOpen);
-
-  if (!navList.classList.contains('opened')) {
+  if (navList.classList.contains('opened')) {
+    toggleIcon.src = iconMenuClose;
+    navToggle.setAttribute('aria-expanded', 'true');
+  } else {
+    toggleIcon.src = iconMenuOpen;
+    navToggle.setAttribute('aria-expanded', 'false');
     navList.removeAttribute('class');
   }
 });
